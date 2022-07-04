8d2adc667f85464d61cc026b2f97cab38c122777
bonnv79 <ngovanbon16@gmail.com>
2022-07-04 14:25:37 +0700

    new-feature: add, edit, delete record

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
 
 reportWebVitals(sendToVercelAnalytics);
---
+++ src/pages/edit.js
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
+import { getUrl } from "../utils";
+
+export default function Edit({ setLoading }) {
+  const [form, setForm] = useState({
+    name: "",
+    position: "",
+    level: "",
+    records: [],
+  });
+  const params = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      const id = params.id.toString();
+      const response = await fetch(getUrl(`/api/record/${params.id.toString()}`));
+
+      if (!response.ok) {
+        const message = `An error has occured: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+
+      const record = await response.json();
+      if (!record) {
+        window.alert(`Record with id ${id} not found`);
+        navigate("/");
+        return;
+      }
+
+      setForm(record);
+      setLoading(false);
+    }
+
+    fetchData();
+
+    return;
+  }, [params.id, navigate, setLoading]);
+
+  function updateForm(value) {
+    return setForm((prev) => {
+      return { ...prev, ...value };
+    });
+  }
+
+  async function onSubmit(e) {
+    setLoading(true);
+    e.preventDefault();
+    const editedPerson = {
+      name: form.name,
+      position: form.position,
+      level: form.level,
+    };
+
+    await fetch(getUrl(`/api/record/update/${params.id}`), {
+      method: "POST",
+      body: JSON.stringify(editedPerson),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+    setLoading(false);
+    navigate("/");
+  }
+
+  return (
+    <div>
+      <h3>Update Record</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            value={form.name}
+            onChange={(e) => updateForm({ name: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="position">Position: </label>
+          <input
+            type="text"
+            className="form-control"
+            id="position"
+            value={form.position}
+            onChange={(e) => updateForm({ position: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="positionOptions"
+              id="positionIntern"
+              value="Intern"
+              checked={form.level === "Intern"}
+              onChange={(e) => updateForm({ level: e.target.value })}
+            />
+            <label htmlFor="positionIntern" className="form-check-label">Intern</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="positionOptions"
+              id="positionJunior"
+              value="Junior"
+              checked={form.level === "Junior"}
+              onChange={(e) => updateForm({ level: e.target.value })}
+            />
+            <label htmlFor="positionJunior" className="form-check-label">Junior</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="positionOptions"
+              id="positionSenior"
+              value="Senior"
+              checked={form.level === "Senior"}
+              onChange={(e) => updateForm({ level: e.target.value })}
+            />
+            <label htmlFor="positionSenior" className="form-check-label">Senior</label>
+          </div>
+        </div>
+        <br />
+
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Update Record"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
