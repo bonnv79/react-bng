af358a377b2e63b4efec032d0fdf77c1cda56bfb
bonnv79 <ngovanbon16@gmail.com>
2022-07-04 11:45:33 +0700

    update .env

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 export const getUrl = (path) => {
-  const SERVER_URL = process.env.WEB_URL || 'https://express-bng-dev.vercel.app';
-  console.log('WEB_URL', process.env.WEB_URL);
+  const SERVER_URL = process.env.REACT_APP_API_URL;
+  console.log('REACT_APP_API_URL', process.env.REACT_APP_API_URL);
   return `${SERVER_URL}${path}`;
 }
